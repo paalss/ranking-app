8d674dbb17012d8a88c63108ff5ce46f3d9db0ad
paalss <paalss94@gmail.com>
2020-09-24 23:38:20 +0200

    Adding and improving tests
    Adding beforeAll and afterAll tests for browser setup and tear down. Adding new test for moving an element down

--- script.test.js
+++ script.test.js
@@ -1,19 +1,19 @@
-/*
-// Unit Test
-const { findFreeLiId } = require('./script')
-test('Able to find a free nubmer?', () => {
-expect(findFreeLiId()).not.toBeNaN()
-})
-*/
-
-// End to end test
 const puppeteer = require('puppeteer')
 
-test('if h2 and a-tags exists', async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+let browser
+let page
 
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: true,
+    slowMo: 250
+  })
+  page = await browser.newPage()
   await page.goto('http://localhost/sider/annet/tingrang/')
+})
+
+test('if h2 and a-tags exists', async () => {
+  // await page.goto('http://localhost/sider/annet/tingrang/')
 
   const h2 = await page.$eval('h2', e => e.innerHTML)
   expect(h2).toBe('Choose list')
@@ -22,25 +22,35 @@ test('if h2 and a-tags exists', async () => {
   expect(aTags[0]).toBe('games')
   expect(aTags[1]).toBe('music')
 
-  browser.close()
 })
 
-test('if lists exists', async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 250 // slow down by 250ms
-  });
-  const page = await browser.newPage()
-
-  await page.goto('http://localhost/sider/annet/tingrang/')
+test('if list exists', async () => {
+  // await page.goto('http://localhost/sider/annet/tingrang/')
 
   // Click first link
   await page.click('a')
 
-  const titles = await page.$$eval('.title', e => e.map(element => element.innerHTML))
-  expect(titles[0]).toBe('Hollow Knight: Silksong')
-  expect(titles[1]).toBe('Noita')
+  // Check if there's a list element with ID 1
 
+  /* selector(#\\3X ) = selecting element by id "X".
+  E.g. selecting item with id 2, will achieved by #\\32 */
+  const titleById1 = await page.$eval('#\\31  .title', e => e.innerHTML)
+  expect(titleById1).not.toBe(null)
+
+}, 10000)
+
+test('if first item can move down', async () => {
+  // await page.goto('http://localhost/sider/annet/tingrang/list.html?list=games')
+
+  const titles = await page.$$eval('.title', e => e.map(e => e.innerHTML))
+
+  await page.click('#\\31  .move-element-down-button')
+
+  const titlesAfterMoveElementDown = await page.$$eval('.title', e => e.map(e => e.innerHTML))
+  expect(titlesAfterMoveElementDown[0]).toBe(titles[1])
+  expect(titlesAfterMoveElementDown[1]).toBe(titles[0])
+}, 10000)
+
+afterAll(async () => {
   browser.close()
-}, 7000)
-
+})
