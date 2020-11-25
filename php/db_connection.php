003841ca94965eea462b55ed10d6877d3a790c79
paalss <paalss94@gmail.com>
2020-11-25 15:07:18 +0100

    Removing all files so I can push the adjusted docker-compose-lamp into this

--- php/db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$db = 'ranking_app';
-
-// connect to MySQL server
-$db_server = new mysqli($host, $username, $password, $db) or die("Could not connect to db!");
