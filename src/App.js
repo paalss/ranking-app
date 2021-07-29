37f1a3ed66238c5cc0bb3c08a9256142528f1bce
paalss <paalss94@gmail.com>
2021-07-29 11:44:13 +0200

    Revert back to mostly empty project. uninstalled express, mysql. Tidyed code and files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import './App.css';
 import ListOfLists from './components/ListOfLists';
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database : 'ranking_app'
-// });
-
-// connection.connect();
-
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
-
-// connection.end();
-
 function App() {
   return (
     <div className="App">
