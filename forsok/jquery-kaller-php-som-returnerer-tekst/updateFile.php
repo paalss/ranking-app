6f1ff5821e6171a59bacab1dd823d0642e4505ba
paalss <paalss94@gmail.com>
2021-07-29 10:28:36 +0200

    removed all code

--- forsok/jquery-kaller-php-som-returnerer-tekst/updateFile.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Bare for å se at vi har tilgang
-$file = file_get_contents('spill.txt');
-echo $file; // dette blir sendt til console.log som result
-
-
--- updateFile.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Bare for å se at vi har tilgang
-// $file = file_get_contents('spill.txt');
-echo $_POST['newOrder']; // dette blir sendt til console.log som data
-
-// Lagre innkommende tekststreng i tekstfila, overskrive det originale
-file_put_contents('spill.txt', $_POST['newOrder']);
-
-
