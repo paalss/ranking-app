cf1a23926a5719807cd35822b345e6467492fb4b
paalss <paalss94@gmail.com>
2021-08-05 15:23:20 +0200

    revert back to mostly empty CRA-project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListOfLists from './components/ListOfLists';
+
+function App() {
+  return (
+    <div className="App">
+      <ListOfLists/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListOfLists.js
@@ -0,0 +1,10 @@
+const ListOfLists = () => {
+  return (
+    <ul className="list-of-lists">
+      <li>best games</li>
+      <li>best movies</li>
+    </ul>
+  );
+};
+
+export default ListOfLists;
